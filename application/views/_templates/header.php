c7bcc5aaeba4e1f57a638706ea26ad1cffab2810
Gerard Paul P. Labitad <gerardpaul.labitad@outlook.com>
2014-10-06 10:39:09 +0800

    Initial Commit

---
+++ application/views/_templates/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>GP Worx | <?php echo $title; ?></title>
+    <meta name="description" content="<?php echo $description;?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?php echo URL; ?>public/img/favicon.ico" type="image/x-icon"/>
+    <!-- css -->
+    <!-- Latest compiled and minified CSS -->
+    <!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"> -->
+    <!-- Optional theme -->
+    <!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"> -->
+    <!-- Font-awesome -->
+    <!-- <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"> -->
+        
+    <link href="<?php echo URL; ?>public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>public/css/bootstrap-theme.min.css" rel="stylesheet">
+    
+    <!-- Fonts -->
+    <link href="<?php echo URL; ?>public/css/font-awesome.min.css" rel="stylesheet">
+    <!--<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>-->
+    <!-- My Styles-->
+    <link href="<?php echo URL; ?>public/css/carousel.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>public/css/cover.css" rel="stylesheet">
+    
+    <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
+    
+    <!-- jQuery -->
+    <!-- <script src="//code.jquery.com/jquery-1.11.0.min.js"></script> -->
+    <!-- <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script> -->  
+    <!-- <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script> -->
+    <script src="<?php echo URL; ?>public/js/jquery-2.1.1.min.js"></script>
+    <script src="<?php echo URL; ?>public/js/bootstrap.min.js"></script>
+    <!-- our JavaScript -->
+    <script src="<?php echo URL; ?>public/js/application.js"></script>
+</head>
+<body>
+<!-- header -->
+<div class="navbar-wrapper">
+    <div class="container">
+        <div class="navbar">
+            <div class="navbar-inner">
+                    <div class="navbar-header">
+                        <button class="navbar-toggle collapsed" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    <a href="#"><div class="navbar-brand logo"></div></a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav pull-right">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                
+            </div>
+            
+        </div>
+    </div>
+</div>
---
+++ application/views/home/index.php
@@ -0,0 +1,41 @@
+<!-- Full Screen Background -->
+<div id="bg">
+    <?php for($i = 1; $i<=1; $i++){ ?>
+    <div>
+        <img src="<?php echo URL; ?>public/img/gpworx_<?php echo $i; ?>.jpg" alt="" class="bg-img">
+    </div>
+    <?php }?>
+</div>
+</div>
+
+<div class="blur"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="center-container">
+                <div class="center-container-inner">
+                    <div class="center-container-content">
+                        <div class="image_div img-left">
+                            <img class="img-circle cover-img img-responsive" src="<?php echo URL; ?>public/img/gerardpaullabitad-gpworks-logo.jpg">
+                        </div>
+                        <div class="image_div img-right">
+                            <img class="img-circle cover-img img-responsive" src="<?php echo URL; ?>public/img/gerardpaullabitad-gpworks-logo.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+<!--            <div class="center-container">
+                <div class="center-content">
+                    <div class="box_container">
+                        <div class="image_holder">
+                            <div class='image_div img-left'><img class='box_image img-circle' src='<?php echo URL;?>public/img/gerardpaullabitad-gptechnologies-logo.jpg' style='width:300px; right: 0;'/></div>
+                            <div class='image_div img-right'><img class='box_image img-circle' src='<?php echo URL;?>public/img/gerardpaullabitad-gptechnologies-logo.jpg' style='width:300px; left: 0;'/></div>
+                            <div class="clear"></div>
+                        </div>
+                        WORKS
+                    </div>
+                </div>
+            </div>-->
+        </div>
+    </div>
+</div>
