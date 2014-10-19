513058209f3b1707ddebbef760712e301fd80b34
Gerard Paul P. Labitad <gerardpaul.labitad@outlook.com>
2014-10-19 08:51:59 +0800

    Revert "Revert "Finished admin functionality.""
    This reverts commit 1a48439c3964485d45c4ed9e4071868bc147da32.

--- application/mysite/models/admin_model.php
+++ application/mysite/models/admin_model.php
@@ -9,41 +9,40 @@ class Admin_Model extends CI_Model {
     private $_username = '';
     private $_password = '';
     private $_salt = '';
-    
     private $_table_name = 'gpworx_admin';
 
     function __construct() {
         parent::__construct();
     }
 
-    function getId(){
+    function getId() {
         return $this->_id;
     }
-    
-    function getFullname(){
+
+    function getFullname() {
         return $this->_fullname;
     }
-    
-    function getProfile1(){
+
+    function getProfile1() {
         return $this->_profile_picture1;
     }
-    
-    function getProfile2(){
+
+    function getProfile2() {
         return $this->_profile_picture2;
     }
-    
-    function getUsername(){
+
+    function getUsername() {
         return $this->_username;
     }
-    
-    function getPassword(){
+
+    function getPassword() {
         return $this->_password;
     }
-    
-    function getSalt(){
+
+    function getSalt() {
         return $this->_salt;
     }
-    
+
     function get_admin($username) {
         $sql = "SELECT * FROM $this->_table_name WHERE username = ?";
         $query = $this->db->query($sql, array($username));
@@ -52,8 +51,21 @@ class Admin_Model extends CI_Model {
         }
         return false;
     }
-    
-    function create_admin($fullname, $username, $password, $salt, $profile1, $profile2){
+
+    function get_admin_details($id) {
+        $sql = "SELECT a.id, a.username, a.password, a.fullname, a.salt,
+                    (SELECT f.file_path FROM gpworx_files f WHERE a.profile_picture1 = f.id) AS \"profile_picture1\",
+                    (SELECT f.file_path FROM gpworx_files f WHERE a.profile_picture2 = f.id) AS \"profile_picture2\"
+                FROM gpworx_admin a
+                WHERE a.id = ? ";
+        $query = $this->db->query($sql, array($id));
+        if ($query->num_rows() > 0) {
+            return $this->createObjectFromData($query->row());
+        }
+        return false;
+    }
+
+    function create_admin($fullname, $username, $password, $salt, $profile1, $profile2) {
         $data = array(
             'fullname' => $fullname,
             'username' => $username,
@@ -62,28 +74,37 @@ class Admin_Model extends CI_Model {
             'profile_picture1' => $profile1,
             'profile_picture2' => $profile2
         );
-        if($this->db->insert($this->_table_name,$data)){
+        if ($this->db->insert($this->_table_name, $data)) {
             return true;
         }
         return false;
     }
-    
-    function update_admin($id, $fullname, $username, $password, $salt, $profile1, $profile2){
-        $data = array(
-            'fullname' => $fullname,
-            'username' => $username,
-            'password' => $password,
-            'salt' => $salt,
-            'profile_picture1' => $profile1,
-            'profile_picture2' => $profile2
-        );
-        
-        if($this->db->update($this->_table_name,$data, array("id" => $id))){
+
+    function update_admin($id, $fullname, $username, $password, $salt, $profile1, $profile2) {
+        if ($password != 'password!') {
+            $data = array(
+                'fullname' => $fullname,
+                'username' => $username,
+                'password' => $password,
+                'salt' => $salt,
+                'profile_picture1' => $profile1,
+                'profile_picture2' => $profile2
+            );
+        }else{
+            $data = array(
+                'fullname' => $fullname,
+                'username' => $username,
+                'profile_picture1' => $profile1,
+                'profile_picture2' => $profile2
+            );
+        }
+
+        if ($this->db->update($this->_table_name, $data, array("id" => $id))) {
             return true;
         }
         return false;
     }
-    
+
     private function createObjectFromData($row) {
         $this->_id = $row->id;
         $this->_fullname = $row->fullname;
@@ -92,7 +113,8 @@ class Admin_Model extends CI_Model {
         $this->_profile_picture1 = $row->profile_picture1;
         $this->_profile_picture2 = $row->profile_picture2;
         $this->_salt = $row->salt;
-        
+
         return $this;
     }
+
 }
